df6ce912c76877fd83d3ff905de31443de550a47
Subhrajyoti <subha.ionic3@gmail.com>
2021-01-20 00:27:15 +0530

    added quaggaJS to read barcodes

--- app.js
+++ app.js
@@ -17,8 +17,42 @@ cameraTrigger.onclick = function () {
   cameraSensor.width = 0;
   cameraSensor.height = 0;
 
-  console.log(cameraOutput.src);
+  // console.log(cameraOutput.src);
+  processImage(cameraOutput.src);
 };
+
+function processImage(imageData) {
+  Quagga.decodeSingle(
+    {
+      decoder: {
+        readers: [
+          "code_128_reader",
+          "ean_reader",
+          "ean_8_reader",
+          "code_39_reader",
+          "code_39_vin_reader",
+          "codabar_reader",
+          "upc_reader",
+          "upc_e_reader",
+          "i2of5_reader",
+          "2of5_reader",
+          "code_93_reader",
+        ], // List of active readers
+      },
+      locate: true, // try to locate the barcode in the image
+      src: imageData, // or 'data:image/jpg;base64,' + data
+    },
+    function (result) {
+      if (result.codeResult) {
+        console.log("result", result.codeResult.code);
+        alert(result.codeResult.code);
+      } else {
+        console.log("not detected");
+        alert("not detected");
+      }
+    }
+  );
+}
 
 function getDevices() {
   // AFAICT in Safari this only gets default devices until gUM is called :/
